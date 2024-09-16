832606a70dd24b385903d275ed791cec922520c4
Andrew Joy Hembrom <135138539+AndrewHembrom@users.noreply.github.com>
2024-09-17 00:20:42 +0530

    login and register

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,13 @@ import Appbar from "./components/Appbar";
 import Home from "./pages/Home/Home";
 import Events from "./pages/Events/Events";
 import Merchandise from "./pages/Merchandise/Merchandise";
+import { UserData } from "./context/UserContext";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Verify from "./pages/auth/Verify";
 
 function App() {
+  const { isAuth } = UserData();
   return (
     <div>
       <BrowserRouter>
@@ -15,6 +20,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/events" element={<Events />} />
           <Route path="/store" element={<Merchandise />} />
+          <Route path="/login" element={isAuth ? <Home /> : <Login />} />
+          <Route path="/register" element={isAuth ? <Home /> : <Register />} />
+          <Route path="/verify" element={isAuth ? <Home /> : <Verify />} />
         </Routes>
       </BrowserRouter>
     </div>
