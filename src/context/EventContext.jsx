286d4c2e68ff6890863cc2cc4246be04ccb611f1
Andrew Joy Hembrom <135138539+AndrewHembrom@users.noreply.github.com>
2024-09-17 06:23:03 +0530

    Final

--- src/context/EventContext.jsx
+++ src/context/EventContext.jsx
@@ -7,6 +7,7 @@ const EventContext = createContext();
 export const EventContextProvider = ({ children }) => {
   const [events, setEvents] = useState([]);
   const [event, setEvent] = useState([]);
+  const [myEvent, setMyEvent] = useState([]);
 
   async function fetchEvents() {
     try {
@@ -26,12 +27,28 @@ export const EventContextProvider = ({ children }) => {
     }
   }
 
+  async function fetchMyEvent() {
+    try {
+      const { data } = await axios.get(`${server}/api/myevents`, {
+        headers: {
+          token: localStorage.getItem("token"),
+        },
+      });
+      setMyEvent(data.events);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   useEffect(() => {
     fetchEvents();
+    fetchMyEvent();
   }, []);
 
   return (
-    <EventContext.Provider value={{ events, fetchEvents, fetchEvent, event }}>
+    <EventContext.Provider
+      value={{ events, fetchEvents, fetchEvent, event, myEvent, fetchMyEvent }}
+    >
       {children}
     </EventContext.Provider>
   );
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -4,13 +4,14 @@ import { EventData } from "../../context/EventContext";
 import EventCard from "../../components/EventCard";
 
 const Dashbord = () => {
-  const { myevent } = EventData();
+  const { myEvent } = EventData();
+  console.log(myEvent);
   return (
     <div className="student-dashboard">
       <h2>All Registered Events</h2>
       <div className="dashboard-content">
-        {myevent && myevent.length > 0 ? (
-          myevent.map((e) => <EventCard key={e._id} event={e} />)
+        {myEvent && myEvent.length > 0 ? (
+          myEvent.map((e) => <EventCard key={e._id} event={e} />)
         ) : (
           <p>No Events Registered Yet</p>
         )}
